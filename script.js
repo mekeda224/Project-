cdfaa9fda218a4698b450f50f254fbb52f383d4a
mekeda224 <mekeda224@gmail.com>
2024-03-26 22:29:35 +0200

    add file

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// const fraction1 = {
+//     numerator: 5,
+//     denominator: 2
+// }
+
+// const fraction2 = {
+//     numerator: 6,
+//     denominator: 3
+// }
+
+// // 1. Функция сложения 2-х объектов-дробей
+// function addition(a, b) {
+//     let rezult = (a.numerator / a.denominator) + (b.numerator / b.denominator);
+//     console.log(rezult);
+// }
+
+// addition(fraction1, fraction2);
+
+// // 2. Функция вычитания 2-х объектов-дробей
+// function subtraction(a, b) {
+//     let rezult = (a.numerator / a.denominator) - (b.numerator / b.denominator);
+//     console.log(rezult);
+// }
+// subtraction(fraction1, fraction2);
+
+// // 3. Функция умножения 2-х объектов-дробей
+
+// function multiplication(a, b) {
+//     let rezult = (a.numerator / a.denominator) * (b.numerator / b.denominator);
+//     console.log(rezult);
+// }
+// multiplication(fraction1, fraction2);
+
+// // 4. Функция деления 2-х объектов-дробей.
+// function division(a, b) {
+//     let rezult = (a.numerator / a.denominator) / (b.numerator / b.denominator);
+//     console.log(rezult);
+// }
+// division(fraction1, fraction2);
+
+
+
